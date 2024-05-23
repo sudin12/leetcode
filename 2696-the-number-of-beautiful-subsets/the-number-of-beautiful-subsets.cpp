91ef96e3e9bb70a6750dba3d5f77b8d88175b8e6
sudin12 <111767963+sudin12@users.noreply.github.com>
2024-05-23 09:29:25 -0700

    Time: 614 ms (32.23%) | Memory: 38.5 MB (44.42%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void backtrack(const vector<int>& nums, int k, unordered_map<int, int>& flag, int pos, int& res) {
+        if (pos == nums.size()) {
+            res++;
+            return;
+        }
+
+        backtrack(nums, k, flag, pos + 1, res);
+
+        int num = nums[pos];
+        if (!flag[num - k]) {
+            flag[num]++;
+            backtrack(nums, k, flag, pos + 1, res);
+            flag[num]--;
+        }
+    }
+
+    int beautifulSubsets(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        unordered_map<int, int> flag;
+        int res = 0;
+        backtrack(nums, k, flag, 0, res);
+        return res - 1;
+    }
+};
